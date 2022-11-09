e41f1dad4d83fa3d75b04968eb9d20d701d43bc8
snehalk1909 <ksnehal1909@gmail.com>
2022-11-09 16:55:55 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import LoginComponent from './components/login/login'
-import DashboardComponent from './components/dashboard/dashboard';
-import HomeComponent from './components/home/home';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginComponent from "./components/login/login";
+import DashboardComponent from "./components/dashboard/dashboard";
+import HomeComponent from "./components/home/home";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/home" element={<HomeComponent />}>
-
-      </Route>
-      <Route path="/login" element={<LoginComponent />}>
-
-      </Route>
-      <Route path="/dashboard" element={<DashboardComponent />}>
-
-      </Route>
+      <Route exact path="/home" element={<HomeComponent />}></Route>
+      <Route path="/login" element={<LoginComponent />}></Route>
+      <Route path="/dashboard" element={<DashboardComponent />}></Route>
     </Routes>
   );
 }
